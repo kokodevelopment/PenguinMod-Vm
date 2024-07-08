46b8fff0cad051882bf551178718a3a5bfb41890
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2024-07-08 12:31:56 -0700

    fix object error

--- src/engine/thread.js
+++ src/engine/thread.js
@@ -63,13 +63,6 @@ class _StackFrame {
          * @type {Object}
          */
         this.executionContext = null;
-
-        /**
-         * Thread vars: for allowing a compiled version of the 
-         * LilyMakesThings Thread Variables extension
-         * @type {Object}
-         */
-        this.variables = Object.create(null);
     }
 
     /**
@@ -214,6 +207,13 @@ class Thread {
          */
         this.procedures = null;
         this.executableHat = false;
+
+        /**
+         * Thread vars: for allowing a compiled version of the 
+         * LilyMakesThings Thread Variables extension
+         * @type {Object}
+         */
+        this.variables = Object.create(null);
     }
 
     /**
